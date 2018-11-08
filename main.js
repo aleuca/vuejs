2ae948ef40c1a9ed10eca98e8f7bb103b8829108
Alina Leuca <aleuca@sbpltg1arhv2j.sbp.lan>
2018-11-08 16:56:04 +0100

    split app into vue components

--- main.js
+++ main.js
@@ -1,31 +1,30 @@
 Vue.component('product', {
+  props: {
+    premium: {
+      type: Boolean,
+      required: true,
+    }
+  },
   template: `
-  <div class="product">
+    <div class="product">
             <h1>{{title}}</h1>
             <div class="product-image">
                 <img :src='image' class='image' />
                 <a :href='link'>
             </div>
-
+            <div>
             <div class="product-info">
-                <div v-for='(variant, index) in variants' :key='variant.variantId' class="color-box" :style='{backgroundColor: variant.variantColor}'
-                    @mouseover='updateProduct(index)'>
-                </div>
-                <ul>
-                    <li v-for="detail in details">{{detail}}</li>
-                </ul>
-                <!-- <p v-show='(variant, index) in variants'>{{inStock}}</p> -->
-                <!-- <p v-else-if="inventory < 10 && inventory > 0">Almost out!</p> -->
-                <p :class="{'out-of-stock': !inStock}">In Stock</p>
-                <p v-if='onSale'>{{onSaleTrue}}</p>
+            <div v-for='(variant, index) in variants' :key='variant.variantId' class="color-box" :style='{backgroundColor: variant.variantColor}'
+                @mouseover='updateProduct(index)'>
             </div>
-
+            <p :class="{'out-of-stock': !inStock}">In Stock</p>
+            <p>Shipping: {{shipping}}</p>
+            <p v-if='onSale'>{{onSaleTrue}}</p>
+            <product-details :details="details"></product-details>
+          </div>
+         </div>
         <button v-on:click="addToCart" :disabled="!inStock" :class="{'disabled-button': !inStock}">Add to Cart</button>
-        <button v-on:click='takeOutOfCart'>delete Item from Cart</button>
-
-        <div class="cart">
-            <p>Cart({{cart}})</p>
-        </div>
+        <button v-on:click='removeFromCart'>delete Item from Cart</button>
     </div>
   `,
   data() {
@@ -55,21 +54,19 @@ Vue.component('product', {
               variantQuantity: 0
             }
           ],
-        cart: 0
       }
     }
   },
 
   methods: {
     addToCart() {
-      this.cart += 1
+      this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId)
+    },
+    removeFromCart() {
+      this.$emit('remove-from-cart', this.variants[this.selectedVariant].variantId)
     },
     updateProduct(index) {
       this.selectedVariant = index
-      console.log(index)
-    },
-    takeOutOfCart() {
-      this.cart -= 1
     },
   },
 
@@ -85,6 +82,12 @@ Vue.component('product', {
     },
     onSaleTrue() {
       return this.brand + ' ' + this.product
+    },
+    shipping() {
+      if (this.premium) {
+        return 'free'
+      }
+      return 2.99
     }
   }
 })
@@ -92,5 +95,44 @@ Vue.component('product', {
 
 let app = new Vue({
   el: '#app',
-  cart: 0
+  data: {
+    premium: false,
+    cart: []
+  },
+  methods: {
+    updateCart(id) {
+      this.cart.push(id)
+    },
+    takeOutOfCart(id) {
+      this.cart.pop(id)
+    },
+  }
+})
+
+Vue.component('product-details', {
+  props: {
+    details: {
+      type: Array,
+      required: true
+    }
+  },
+  template:
+    `
+      <ul>
+        <li v-for="detail in details">{{detail}}</li>
+      </ul>
+  `,
+  data() {
+    return {
+      details
+    }
+  }
+})
+
+let productDetails = new Vue({
+  el: '#product-info',
+  data: {
+    datails:
+      ["80%cotton", "20% polyester"]
+  }
 })
